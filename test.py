50c3c49fa8ada335a0b9b25e67bb356659b68661
CohleM <manish981194@gmail.com>
2019-10-15 22:11:46 +0545

    Algorithm still to be checked,has some flaws , loop's not breaking

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-import pygame
 import random
 from collections import defaultdict
-pygame.init()
-
-GameMode = True
-margin = 10
-gap = 175
-win = pygame.display.set_mode((560,560))
-pygame.display.set_caption('MazeGeneration')
 edges = list()
 tree = defaultdict(list)
 count = 0
@@ -36,11 +28,13 @@ while len(tree) != 1:
             if c == edges[a][0]: 
                 t1 = (b,c)
                 break
+            break
     for d in tree:
         for e in tree[d]:
             if e == edges[a][1]:
                 t2 = (d,e,tree[d])
                 break
+            break
     if t1[0] == t2[1]: continue
     print(edges)
     for q in t2[2]:
@@ -53,22 +47,5 @@ while len(tree) != 1:
     print('\n')
     print(len(edges),len(tree) , a )
 
-
-
-while GameMode:
-    pygame.time.delay(10)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            GameMode = False
-    color = (255,255,255)
-    #pygame.draw.rect(win,color,((margin + gap)*0 + margin ,(margin + gap)*0 + margin ),gap,gap)
-
-
-    for row in range(3):
-        for column in range(3):
-            pygame.draw.rect(win,color,((margin + gap)*column + margin,
-                (margin + gap)*row + margin,
-                gap,
-                gap))
-            pygame.display.update()
-pygame.quit()
+for key,value in tree.items():
+    print(key , ':', value)
